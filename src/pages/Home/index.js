98024116634d71ddcc2cc21f82c77400f9987eab
GIT-Jorgin <61719584+GIT-Jorgin@users.noreply.github.com>
2021-02-16 17:11:45 -0300

    Sidenav / Music Player

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,215 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Slider from '@material-ui/core/Slider';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { 
+    Body,
+    SideNav,
+    LogoIMG,
+    SideNavContainer,
+    MenuGroup,
+    MenuGroupTitle,
+    MenuItemList,
+    MenuItem,
+    MenuItemIcon,
+    MenuItemText,
+    MenuItemActive,
+    MusicPlayer,
+    MenuPlaylist,
+    PlayingSong,
+    PlayingSongAlbum,
+    PlayingSongName,
+    MusicName,
+    ArtistName,
+    MusicController,
+    ControllerIcon,
+    MusicProgress,
+    MusicProgressCount,
+    MusicVolume,
+    MainContainer
+} from './styles';
+import Logo from '../../images/logo.png';
+import IconExplore from '../../images/icon_explore.png';
+import icon_hear_this from '../../images/icon_hear_this.png';
+import IconRadio from '../../images/icon_radio.png';
+import IconLastAdded from '../../images/icon_last_added.png';
+import IconFavoriteSongs from '../../images/icon_favorite_songs.png';
+import IconArtists from '../../images/icon_artists.png';
+import IconSongs from '../../images/icon_songs.png';
+import IconGenres from '../../images/icon_genres.png';
+import IconPlaylist from '../../images/icon_playlist.png';
+import IconAddPlaylist from '../../images/icon_add_playlist.png';
+import Album from '../../images/album.png'
+import IconShuffle from '../../images/icon_shuffle.png';
+import IconBack from '../../images/icon_back.png';
+import ButtonPause from '../../images/button_pause.png';
+import IconNext from '../../images/icon_next.png';
+import IconReplay from '../../images/icon_replay.png';
+import IconVolume from '../../images/icon_volume.png';
 
 export default function Home(){
+
+
+      const ProgressMusicSlider = withStyles({
+        root: {
+          marginTop: 3,
+          color: '#1DE9B6',
+          height: 8,
+          width: 270
+        },
+        thumb: {
+          height: 14,
+          width: 14,
+          marginTop: -5,
+          marginLeft: -12,
+          '&:focus, &:hover, &$active': {
+            boxShadow: 'inherit',
+          },
+        },
+        active: {},
+        valueLabel: {
+          left: 'calc(-50% + 4px)',
+        },
+        track: {
+          height: 4,
+          borderRadius: 4,
+        },
+        rail: {
+          height: 4,
+          borderRadius: 4,
+        },
+      })(Slider);
+      
+      const MusicVolumeSlider = withStyles({
+        root: {
+          marginTop: 3,
+          color: '#1DE9B6',
+          height: 8,
+          width: 120,
+        },
+        thumb: {
+          height: 14,
+          width: 14,
+          marginTop: -5,
+          marginLeft: -12,
+          '&:focus, &:hover, &$active': {
+            boxShadow: 'inherit',
+          },
+        },
+        active: {},
+        valueLabel: {
+          left: 'calc(-50% + 4px)',
+        },
+        track: {
+          height: 4,
+          borderRadius: 4,
+        },
+        rail: {
+          height: 4,
+          borderRadius: 4,
+        },
+      })(Slider);
+
     return(
-        <h1>OLA MUNDO</h1>
+        <Body>
+            <MusicPlayer>
+                <PlayingSong>
+                    <PlayingSongAlbum src={Album} />
+                    <PlayingSongName>
+                        <MusicName>MOOD</MusicName>
+                        <ArtistName>24kGoldn</ArtistName>
+                    </PlayingSongName>
+                </PlayingSong>
+                <MusicController>
+                    <ControllerIcon src={IconShuffle} />
+                    <ControllerIcon src={IconBack} />
+                    <ControllerIcon src={ButtonPause} />
+                    <ControllerIcon src={IconNext} />
+                    <ControllerIcon src={IconReplay} />
+                </MusicController>
+                <MusicProgress>
+                    <MusicProgressCount>0:45</MusicProgressCount>
+                    <ProgressMusicSlider aria-label="pretto slider" defaultValue={40} />
+                    <MusicProgressCount>2:20</MusicProgressCount>
+                </MusicProgress>
+                <MusicVolume>
+                    <ControllerIcon src={IconVolume} />
+                    <MusicVolumeSlider aria-label="pretto slider" defaultValue={55} />
+                </MusicVolume>
+            </MusicPlayer>
+            <SideNav>
+                <SideNavContainer>
+                    <LogoIMG alt="Spotify" src={Logo} />
+                    <MenuGroup style={{marginTop: 30}}>
+                        <MenuGroupTitle>PROCURAR MUSICA</MenuGroupTitle>
+                        <MenuItemList>
+                            <MenuItem style={{marginTop: 5}}>
+                                <MenuItemIcon src={IconExplore} />
+                                <MenuItemText>Explorar</MenuItemText>
+                            </MenuItem>
+                            <MenuItemActive>
+                                <MenuItemIcon src={icon_hear_this} />
+                                <MenuItemText style={{color: 'rgba(29, 233, 182, 1)'}}>Ouça agora</MenuItemText>
+                            </MenuItemActive>
+                            <MenuItem>
+                                <MenuItemIcon src={IconRadio} />
+                                <MenuItemText>Radio</MenuItemText>
+                            </MenuItem>
+                        </MenuItemList>
+                    </MenuGroup>
+                    <MenuGroup style={{marginTop: 20}}>
+                        <MenuGroupTitle>SUA BIBLIOTECA</MenuGroupTitle>
+                        <MenuItemList>
+                            <MenuItem style={{marginTop: 5}}>
+                                <MenuItemIcon src={IconLastAdded} />
+                                <MenuItemText>Ultimas adicionadas</MenuItemText>
+                            </MenuItem>
+                            <MenuItem>
+                                <MenuItemIcon src={IconFavoriteSongs} />
+                                <MenuItemText>Favoritas</MenuItemText>
+                            </MenuItem>
+                            <MenuItem>
+                                <MenuItemIcon src={IconArtists} />
+                                <MenuItemText>Artistas</MenuItemText>
+                            </MenuItem>
+                            <MenuItem>
+                                <MenuItemIcon src={IconSongs} />
+                                <MenuItemText>Musicas</MenuItemText>
+                            </MenuItem>
+                            <MenuItem>
+                                <MenuItemIcon src={IconGenres} />
+                                <MenuItemText>Generos</MenuItemText>
+                            </MenuItem>
+                        </MenuItemList>
+                    </MenuGroup>
+                    <MenuGroup style={{marginTop: 20, paddingBottom: 60}}>
+                        <MenuPlaylist>
+                            <MenuGroupTitle>PLAYLISTS</MenuGroupTitle>
+                            <MenuItemIcon src={IconAddPlaylist} />
+                        </MenuPlaylist>
+                        <MenuItemList>
+                            <MenuItem style={{marginTop: 5}}>
+                                <MenuItemIcon src={IconPlaylist} />
+                                <MenuItemText>Sextou</MenuItemText>
+                            </MenuItem>
+                            <MenuItem>
+                                <MenuItemIcon src={IconPlaylist} />
+                                <MenuItemText>De volta à infância</MenuItemText>
+                            </MenuItem>
+                            <MenuItem>
+                                <MenuItemIcon src={IconPlaylist} />
+                                <MenuItemText>Meu Top-100</MenuItemText>
+                            </MenuItem>
+                            <MenuItem>
+                                <MenuItemIcon src={IconPlaylist} />
+                                <MenuItemText>Treino pesado</MenuItemText>
+                            </MenuItem>
+                        </MenuItemList>
+                    </MenuGroup>
+                </SideNavContainer>
+            </SideNav>
+            <MainContainer>
+                
+            </MainContainer>
+        </Body>
     )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,2 +1,257 @@
 import styled from 'styled-components';
+import SFProTextBold from '../../fonts/FontsFree-Net-SFProText-Bold.ttf';
+import SFProTextLight from '../../fonts/FontsFree-Net-SFProText-Light.ttf';
+import SFProTextMedium from '../../fonts/FontsFree-Net-SFProText-Medium-1.ttf';
+import SFProTextRegular from '../../fonts/FontsFree-Net-SFProText-Regular-1.ttf';
+import SFProTextSemibold from '../../fonts/FontsFree-Net-SFProText-Semibold-1.ttf';
 
+export const Body = styled.div`
+    @font-face {
+        font-family: 'SF Pro Text Light';
+        src: local('SF Pro Text Light'), url(${SFProTextLight}) format('truetype');
+        font-weight: 300;
+        font-style: normal;
+    }
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    display: grid;
+    height: 100vh;
+    grid-template-columns: 305px 1fr;
+    margin: 0;
+    padding: 0;
+    outline: 0;
+`;
+
+export const SideNav = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: calc(100% - 80px);
+    background: rgb(0,39,64);
+    background: linear-gradient(302deg, rgba(0,39,64,1) 0%, rgba(1,63,94,1) 85%);
+    overflow-y: auto;
+    overflow-x: hidden;
+/* width */
+&::-webkit-scrollbar {
+  width: 5px;
+}
+
+/* Track */
+&::-webkit-scrollbar-track {
+  border-radius: 100px;
+}
+ 
+/* Handle */
+&::-webkit-scrollbar-thumb {
+  background: #B0A5A8; 
+}
+
+/* Handle on hover */
+&::-webkit-scrollbar-thumb:hover {
+  background: #A5999D;
+}
+`;
+
+export const SideNavContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+    height: 92%;
+`;
+
+export const LogoIMG = styled.img`
+    width: 106px;
+    height: 32px;
+
+`;
+
+export const MenuGroup = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+`;
+
+export const MenuGroupTitle = styled.h5`
+    @font-face {
+        font-family: 'SF Pro Text SemiBold';
+        src: local('SF Pro Text SemiBold'), url(${SFProTextSemibold}) format('truetype');
+        font-weight: 600;
+        font-style: normal;
+    }
+
+    color: #FFFF;
+    font-family: 'SF Pro Text SemiBold';
+    font-size: 14px;
+    letter-spacing: 1px;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+`;
+
+export const MenuItemList = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+`;
+
+export const MenuItem = styled.div`
+    margin-top: 16px;
+    height: 20px;
+    align-items: center;
+    padding-left: 30px;
+    display: flex;
+    align-items: center;
+`;
+
+export const MenuItemActive = styled.div`
+    margin-top: 16px;
+    display: flex;
+    width: 80%;
+    padding: 10px;
+    height: 20px;
+    padding-left: 30px;
+    border-radius: 100px;
+    align-items: center;
+    background-color: rgba(29, 233, 182, 0.15);
+`;
+
+export const MenuItemIcon = styled.img`
+    cursor: pointer;
+`;
+
+export const MenuPlaylist = styled.div`
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const MenuItemText = styled.p`
+    @font-face {
+        font-family: 'SF Pro Text Regular';
+        src: local('SF Pro Text Regular'), url(${SFProTextRegular}) format('truetype');
+        font-weight: 400;
+        font-style: normal;
+    }
+    cursor: pointer;
+    font-family: 'SF Pro Text Regular';
+    color: rgba(255, 255, 255, 0.8);
+    font-size: 14px;
+    margin-left: 15px;
+    line-height: 5px;
+`;
+
+export const MusicPlayer = styled.div`
+    display: flex;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    bottom: 0;
+    left: 0;
+    width: 100vw;
+    height: 80px;
+    background: rgba(255, 255, 255, 0.85);
+`;
+
+export const PlayingSong = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const PlayingSongAlbum = styled.img`
+    cursor: pointer;
+    border-radius: 2px;
+`;
+
+export const PlayingSongName = styled.div`
+    display: block;
+    margin-top: -3px;
+    line-height: 1px;
+    margin-left: 10px;
+`;
+
+export const MusicName = styled.h4`
+    @font-face {
+        font-family: 'SF Pro Text Bold';
+        src: local('SF Pro Text Bold'), url(${SFProTextBold}) format('truetype');
+        font-weight: bold;
+        font-style: normal;
+    }
+
+    font-family: 'SF Pro Text Bold';
+    font-size: 14px;
+    color: #1C1E25;
+    line-height: 1px;
+`;
+
+export const ArtistName = styled.p`
+    @font-face {
+        font-family: 'SF Pro Text Regular';
+        src: local('SF Pro Text Regular'), url(${SFProTextRegular}) format('truetype');
+        font-weight: 400;
+        font-style: normal;
+    }
+
+    font-family: 'SF Pro Text Regular';
+    line-height: 1px;
+    font-size: 14px;
+    color: #51596D;
+`;
+
+export const MusicController = styled.div`
+    display: flex;
+    width: 22%;
+    padding-left: 8%;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const ControllerIcon = styled.img`
+    cursor: pointer;
+    padding: 10px;
+    border-radius: 100px;
+
+    &:hover{
+        background: rgba(0, 0, 0, 0.1)
+    }
+`;
+
+export const MusicProgress = styled.div`
+    display: flex;
+    width: 25%;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const MusicProgressCount = styled.h1`
+    @font-face {
+            font-family: 'SF Pro Text Light';
+            src: local('SF Pro Text Light'), url(${SFProTextLight}) format('truetype');
+            font-weight: 300;
+            font-style: normal;
+        }
+    font-family: 'SF Pro Text Light';
+    font-size: 12px;
+    color: rgba(81, 89, 109, 0.8);
+`;
+
+export const MusicVolume = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+export const MainContainer = styled.div`
+    display: grid;
+    grid-template-rows: 3fr 1fr 1fr;
+    background: rgba(250, 251, 254, 1);
+`;
+
+export const Header = styled.div`
+    width: 100%;
+    height: 100%;
+    background: rgb(29,233,182);
+    background: linear-gradient(142deg, rgba(29,233,182,1) 0%, rgba(0,182,234,1) 85%);
+
+`;
